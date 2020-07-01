a67693cf3b4e950d30eb449df167a47fdb2e1110
christianraymond <ombenisn.raymond@gmail.com>
2020-07-01 12:54:57 +0200

    added hoc

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import classes from './BurgerIngredients.module.css'
+import PropTypes from 'prop-types'; // ES6
+
+
+class BurgerIngredient extends Component {
+    render() {
+
+        let ingredients = null;
+
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredients = <div className={classes.BreadBottom}></div>;
+                break;
+            case ('bread-top'):
+                ingredients = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seed1}></div>
+                        <div className={classes.Seeds2}></div>
+                    </div>
+                );
+                break;
+            case ('cheese'):
+                ingredients = <div className={classes.Cheese}></div>;
+                break;
+
+            case ('salad'):
+                ingredients = <div className={classes.Salad}></div>;
+                break;
+
+            case ('bacon'):
+                ingredients = <div className={classes.Bacon}></div>;
+                break;
+            case ('meat'):
+                ingredients = <div className={classes.Meat}></div>;
+                break;
+                
+            default:
+                ingredients = null;
+        }
+        return ingredients;
+    }
+}
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import classes from './Modal.module.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop'
+
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.showModal !== this.props.showModal;
+    }
+
+    componentDidUpdate(){
+        console.log('[Modal] WillUpdate@')
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop showModal={this.props.showModal} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.showModal ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
